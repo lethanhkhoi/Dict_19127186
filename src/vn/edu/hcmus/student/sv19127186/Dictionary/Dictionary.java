de0c9e7a3a5998c98797258ba433645b58a2f1a4
LeThanhKhoi <lethanhkhoi081001@gmail.com>
2021-12-13 21:57:36 +0700

    setup

---
+++ src/vn/edu/hcmus/student/sv19127186/Dictionary/Dictionary.java
@@ -0,0 +1,10 @@
+package vn.edu.hcmus.student.sv19127186.Dictionary;
+
+/**
+ * vn.edu.hcmus.student.sv19127186.Dictionary
+ * Created by 84904
+ * Date 13/12/2021 - 9:53 PM
+ * Description: ...
+ */
+public class Dictionary {
+}
---
+++ src/vn/edu/hcmus/student/sv19127186/Dictionary/Main.java
@@ -0,0 +1,8 @@
+package vn.edu.hcmus.student.sv19127186.Dictionary;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
